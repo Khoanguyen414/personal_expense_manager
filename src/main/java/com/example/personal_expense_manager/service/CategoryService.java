9346dab24ea4cd8e3e7f5483e4256168a1d895dd
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-11-15 23:47:27 +0700

    fea(service)t: Add CategoryService with CRUD logic

---
+++ src/main/java/com/example/personal_expense_manager/service/CategoryService.java
@@ -0,0 +1,62 @@
+package com.example.personal_expense_manager.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.personal_expense_manager.dto.request.CategoryRequest;
+import com.example.personal_expense_manager.dto.response.CategoryResponse;
+import com.example.personal_expense_manager.entity.Category;
+import com.example.personal_expense_manager.exception.ResourceNotFoundException;
+import com.example.personal_expense_manager.mapper.CategoryMapper;
+import com.example.personal_expense_manager.repository.CategoryRepository;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CategoryService {
+    CategoryRepository categoryRepository;
+    CategoryMapper categoryMapper;
+
+    public CategoryResponse createCategory(CategoryRequest request) {
+        Category category = categoryMapper.toCategory(request);
+        return categoryMapper.toCategoryResponse(categoryRepository.save(category));
+    }
+
+    public CategoryResponse getCategoryById(int id) {
+        Category category = categoryRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Category not found with id: " + id));
+        
+        return categoryMapper.toCategoryResponse(category);
+    }
+
+    public List<CategoryResponse> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+
+        return categories.stream().map(categoryMapper::toCategoryResponse).toList();
+    }
+
+    public CategoryResponse updateCategory(int id, CategoryRequest request) {
+        Category existingCategory = categoryRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Category not found with id: " + id));
+
+        existingCategory.setName(request.getName());
+        existingCategory.setDescription(request.getDescription());
+
+        Category updatedCategory = categoryRepository.save(existingCategory);
+
+        return categoryMapper.toCategoryResponse(updatedCategory);  
+    }
+
+    public void deleteCategory(int id) {
+        Category category = categoryRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Category not found with id: " + id));
+
+        categoryRepository.delete(category);
+    }
+}   
