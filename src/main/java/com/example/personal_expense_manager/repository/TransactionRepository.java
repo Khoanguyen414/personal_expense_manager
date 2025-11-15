885d8fa060cd38eb2de84a9c9a1fb56b76e106db
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-11-15 22:43:01 +0700

    feat: Add Category and Transaction repositories

---
+++ src/main/java/com/example/personal_expense_manager/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.example.personal_expense_manager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.personal_expense_manager.entity.Transaction;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Integer>{
+
+}
