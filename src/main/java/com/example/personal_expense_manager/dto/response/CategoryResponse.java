4d7ec9d74abec1569727d31316db8bbc86af5a18
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-11-15 22:58:25 +0700

    feat(dto): Add CategoryRequest and CategoryResponse

---
+++ src/main/java/com/example/personal_expense_manager/dto/response/CategoryResponse.java
@@ -0,0 +1,19 @@
+package com.example.personal_expense_manager.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CategoryResponse {
+    int id;
+    String name;
+    String description;
+}
