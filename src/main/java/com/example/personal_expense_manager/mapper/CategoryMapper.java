9cfb51352a88c9f0e5bb9b37b36f5c7f17a045c4
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-11-16 23:52:51 +0700

    refactor(mapper): Configure CategoryMapper to ignore unused fields and clean warnings

--- src/main/java/com/example/personal_expense_manager/mapper/CategoryMapper.java
+++ src/main/java/com/example/personal_expense_manager/mapper/CategoryMapper.java
@@ -1,6 +1,7 @@
 package com.example.personal_expense_manager.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.example.personal_expense_manager.dto.request.CategoryRequest;
 import com.example.personal_expense_manager.dto.response.CategoryResponse;
@@ -8,6 +9,8 @@ import com.example.personal_expense_manager.entity.Category;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "transactions", ignore = true)
     Category toCategory(CategoryRequest request);
     CategoryResponse toCategoryResponse(Category category);
 }
