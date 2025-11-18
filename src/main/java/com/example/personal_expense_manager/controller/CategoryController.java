d3aa262621e777ee1a3ac1338ca5ff8eae5d8d8b
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-11-18 23:15:02 +0700

    fix(controller): Correct typo in Transaction API path and clean up code

--- src/main/java/com/example/personal_expense_manager/controller/CategoryController.java
+++ src/main/java/com/example/personal_expense_manager/controller/CategoryController.java
@@ -22,8 +22,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-
 @RestController
 @RequestMapping("/api/categories")
 @RequiredArgsConstructor
--- src/main/java/com/example/personal_expense_manager/repository/CategoryRepository.java
+++ src/main/java/com/example/personal_expense_manager/repository/CategoryRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.example.personal_expense_manager.entity.Category;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long>{
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
