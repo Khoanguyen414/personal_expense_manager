78cbd0613692073ddfe5b0785f701abcee77306c
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-11-21 12:17:45 +0700

    feat(repo): Add custom JPQL query to calculate statistics by category

--- src/main/java/com/example/personal_expense_manager/repository/TransactionRepository.java
+++ src/main/java/com/example/personal_expense_manager/repository/TransactionRepository.java
@@ -1,11 +1,26 @@
 package com.example.personal_expense_manager.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.example.personal_expense_manager.dto.response.ExpenseStatResponse;
 import com.example.personal_expense_manager.entity.Transaction;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long>{
-
+    @Query("SELECT new com.example.personal_expense_manager.dto.response.ExpenseStatResponse(" +
+        "SUM(t.amount), " +
+        "t.type, " +
+        "c.id, " +
+        "c.name) " +
+        "FROM Transaction t JOIN t.category c " +
+        "WHERE t.date BETWEEN :startDate AND :endDate " +
+        "GROUP BY c.id, c.name, t.type" 
+    )
+    List<ExpenseStatResponse> getExpenseStatistic(@Param("start") LocalDate start, @Param("end") LocalDate end);
 }
